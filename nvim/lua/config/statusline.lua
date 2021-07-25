c81a15a6996fb87fd1b54b66ead2fd322936a00b
Rohit K Viswanath <kvrohit@gmail.com>
2021-07-25 15:57:16 +0530

    Tweak mode colors for status line

--- nvim/lua/config/statusline.lua
+++ nvim/lua/config/statusline.lua
@@ -7,11 +7,11 @@ M.setup = function()
       filename_section = "right",
     },
     mode_colors = {
-      n = "#659ea2",
+      n = "#8296b0",
       i = "#a18daf",
       c = "#ab924c",
-      v = "#8296b0",
-      V = "#8296b0",
+      v = "#659ea2",
+      V = "#659ea2",
     },
     mode_icons = {
       n = "N",
